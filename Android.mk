eb0b4b942131883cf6bd60e31c7dc52ef0885f71
Vishal Mahaveer <vishalm@ti.com>
2010-08-02 17:52:44 -0500

    WLAN - Hotspot/softAP: netd changes for softAP commands
    Implementing netd SOftAP APIs for working with TI solution
    
    Change-Id: I34c2d776d7173062fea35699c8f212352166e34d

--- Android.mk
+++ Android.mk
@@ -9,6 +9,31 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifdef OMAP_ENHANCEMENT
+DK_ROOT = hardware/ti/wlan/wl1271_softAP
+OS_ROOT = $(DK_ROOT)/platforms
+STAD    = $(DK_ROOT)/stad
+UTILS   = $(DK_ROOT)/utils
+TWD     = $(DK_ROOT)/TWD
+COMMON  = $(DK_ROOT)/common
+TXN     = $(DK_ROOT)/Txn
+CUDK    = $(DK_ROOT)/CUDK
+
+WILINK_INCLUDES = $(STAD)/Export_Inc               \
+                  $(STAD)/src/Application          \
+                  $(UTILS)                         \
+                  $(OS_ROOT)/os/linux/inc          \
+                  $(OS_ROOT)/os/common/inc         \
+                  $(TWD)/TWDriver                  \
+                  $(TWD)/FirmwareApi               \
+                  $(TWD)/TwIf                      \
+                  $(TWD)/FW_Transfer/Export_Inc    \
+                  $(TXN)                           \
+                  $(CUDK)/configurationutility/inc \
+                  $(CUDK)/hostapd                  \
+                  $(CUDK)/os/common/inc
+endif
+
 LOCAL_SRC_FILES:=                                      \
                   main.cpp                             \
                   CommandListener.cpp                  \
@@ -20,7 +45,6 @@ LOCAL_SRC_FILES:=                                      \
                   NatController.cpp                    \
                   PppController.cpp                    \
                   PanController.cpp                    \
-                  SoftapController.cpp                 \
                   UsbController.cpp                    \
                   ThrottleController.cpp
 
@@ -37,6 +61,15 @@ LOCAL_CFLAGS += -DWIFI_DRIVER_FW_STA_PATH=\"$(WIFI_DRIVER_FW_STA_PATH)\"
 endif
 ifdef WIFI_DRIVER_FW_AP_PATH
 LOCAL_CFLAGS += -DWIFI_DRIVER_FW_AP_PATH=\"$(WIFI_DRIVER_FW_AP_PATH)\"
+endif
+
+ifdef OMAP_ENHANCEMENT
+LOCAL_CFLAGS += -D__BYTE_ORDER_LITTLE_ENDIAN
+LOCAL_STATIC_LIBRARIES := libhostapdcli
+LOCAL_C_INCLUDES += $(WILINK_INCLUDES)
+LOCAL_SRC_FILES += SoftapControllerTI.cpp
+else
+LOCAL_SRC_FILES += SoftapController.cpp
 endif
 
 LOCAL_SHARED_LIBRARIES := libsysutils libcutils libnetutils libcrypto
